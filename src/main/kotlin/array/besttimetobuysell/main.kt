896ca94927f803fc67c4aba27e2a90064bb00c97
Andhika Yuana <andhikayuana@gmail.com>
2022-11-23 15:20:13 +0700

    chore: update best time to buy sell

--- src/main/kotlin/array/besttimetobuysell/main.kt
+++ src/main/kotlin/array/besttimetobuysell/main.kt
@@ -3,13 +3,7 @@ package array.besttimetobuysell
 fun main() {
 
     val input1 = intArrayOf(7, 1, 5, 3, 6, 4)
-    val expected1 = 7
-
-    val input2 = intArrayOf(1, 2, 3, 4, 5)
-    val expected2 = 4
-
-    val input3 = intArrayOf(7, 6, 4, 3, 1)
-    val expected3 = 0
+    val expected1 = 5
 
     val actual = maxProfit(input1)
     println(actual)
@@ -17,11 +11,14 @@ fun main() {
 }
 
 fun maxProfit(prices: IntArray): Int {
+    var buy = prices[0]
     var maxProfit = 0
 
     for (i in 1 until prices.size) {
-        if (prices[i] > prices[i - 1]) {
-            maxProfit += prices[i] - prices[i - 1]
+        if (buy > prices[i]) {
+            buy = prices[i]
+        } else if ((prices[i] - buy) > maxProfit) {
+            maxProfit = prices[i] - buy
         }
     }
 
